ee08f96dad5c7723aea641aa7249bbb331839ef0
Itay Mevorach <126098931+itaymev@users.noreply.github.com>
2024-04-08 15:01:02 -0700

    Update 9-palindrome-number.py

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -2,26 +2,16 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        
-        x = str(x)
 
-        # print(x)
-        
-        if len(x) == 1:
-            return True
-        elif len(x) == 2:
-            if x[0] == x[len(x)-1]:
-                return True
-            else:
-                return False
-        else:
-            if x[0] == x[len(x)-1]:
-                if len(str(int(x[1:len(x)-1]))) < len(x[1:len(x)-1]):
-                    return False
-                else:
-                    return self.isPalindrome(int(x[1:len(x)-1]))
-            else:
-                return False 
+        reversed_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+
+        return reversed_num == x
             
 solution = Solution()
 
